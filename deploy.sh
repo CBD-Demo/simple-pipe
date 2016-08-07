87053938db2006069387a7aa16d698321902ab4c
Soheil Qanbari <soheil@infosys.tuwien.ac.at>
2016-08-07 23:19:17 +0200

    d

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker kill app > /dev/null 2>&1
 docker rm app > /dev/null 2>&1
-docker run -d --name app -it -p 9999:9090 emasos/bizops
+docker run -d --name app -p 9999:8080 emasos/bizops
 sleep 3
 curl -v http://localhost:9999
