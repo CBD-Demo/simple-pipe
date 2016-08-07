498b18a0acff44395efcdbc9c22d35377000c8b6
Soheil Qanbari <soheil@infosys.tuwien.ac.at>
2016-08-07 21:04:01 +0200

    Original

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 # Sample App that depends on a standard base image
-FROM kohsuke/acmecorp-buildenv
+FROM uday/acmecorp-base
 
 MAINTAINER Udaypal Aarkoti <uaarkoti@gmail.com>
 
 COPY target/test-webapp.war /usr/local/tomcat/webapps/ROOT.war
 RUN rm -rf /usr/local/tomcat/webapps/ROOT
+
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker kill app > /dev/null 2>&1
 docker rm app > /dev/null 2>&1
-docker run -d --name app -p 9898:9090 kohsuke/acmecorp-buildenv
+docker run -d --name app -p 9999:9090 uday/acmecorp-app
 sleep 3
-curl -v http://localhost:9898
+curl -v http://localhost:9999
