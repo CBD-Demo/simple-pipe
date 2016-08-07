cb04f1755e18c5dfce283bf68187dadc22e9938f
Soheil Qanbari <soheil@infosys.tuwien.ac.at>
2016-08-07 20:58:36 +0200

    Update port

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker kill app > /dev/null 2>&1
 docker rm app > /dev/null 2>&1
-docker run -d --name app -p 9999:9090 kohsuke/acmecorp-buildenv
+docker run -d --name app -p 9191:9090 kohsuke/acmecorp-buildenv
 sleep 3
-curl -v http://localhost:9999
+curl -v http://localhost:9191
